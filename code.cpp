fe7f4046db52bf7d03189f7b22abb5d48beb5bbf
ujjwal13818 <114792389+ujjwal13818@users.noreply.github.com>
2024-01-14 12:21:38 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -31,7 +31,8 @@ using namespace std;
 #define maxpq priority_queue<int>
 #define ff first
 #define ss second
-#define vHas(v,x) find(v.begin(), v.end(), x) != v.end()
+#define vHas(v,x) (find(v.begin(), v.end(), x) != v.end())
+#define setHas(s,x) (s.find(x) != s.end())
 
 
 typedef long long ll;
@@ -40,6 +41,8 @@ typedef long double lld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef map<int,int> mii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll>> vvll;
 
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
@@ -78,27 +81,28 @@ ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
 bool revsort(ll a, ll b) {return a > b;}
 ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
 void google(int t) {cout << "Case #" << t << ": ";}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
 ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
 ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
 ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-// vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
 void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
 vector<int> primeFact(int n) { vector<int> v; for (int i = 2; i <= n; i++) { if (n % i == 0) v.push_back(i); while (n % i == 0) n /= i; } return v; }
 int lb(vi &v, int x) { int low = 0, high = v.size(), ans = -1; while (low < high) { int mid = low + (high - low) / 2; if (v[mid] >= x) high = mid - 1; else { ans = mid; low = mid + 1; } } if (low <= v.size() - 1 && v[low] < x) ans++; return ans; }
 //gives the index at which value is just shorter.
 int ub(vi &v, int x) { int low = 0, high = v.size(), ans = high; while (low < high) { int mid = low + (high - low) / 2; if (v[mid] <= x) low = mid + 1; else { ans = mid; high = mid - 1; } } if (high >= 0 && v[high] > x) ans--; return ans; }
 //gives the index at which  value is just greater.
-
-
+//gives sum of vector from index a to b both inclusive;
+ll findSum(const vector<ll> &v, ll a , ll b) {if(a < 0 || b > v.size())return -1; ll ts = 0 ; frn(i,a,b+1)ts += v[i]; return ts; };
+//KMP algo
+vector<ll> KMP(string &s, string &t) { int n = s.size(), m = t.size(); vector<ll> lps(m, 0); int l = 0; for(int r = 1; r < m; r++) { if(t[l] == t[r]) { lps[r] = l + 1; l++; } else { if(l != 0) { l = lps[l - 1]; } else { lps[r] = 0; } } } vector<ll> ans; int j = 0; for(int i = 0; i < n; i++) { if(t[j] == s[i]) j++; else { if(j != 0) { j = lps[j - 1]; i--; } } if(j == m) { ans.pb(i - m + 1); j = 0; } } return ans; }
 
 
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("Error.txt", "w", stderr);
 #endif
-
-	return 0;
+  
+ return 0;
 }
